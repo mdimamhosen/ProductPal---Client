e5d898d88ee06e8c0a4060913aa3a42203b4b95d
mdimamhosen <imamhosen9426224@gmail.com>
2024-08-17 19:30:32 +0600

    route defines

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,7 +1,6 @@
 export const getToken = () => {
   return localStorage.getItem("token") || sessionStorage.getItem("token");
 };
-
 export const setToken = () => {
   return localStorage.setItem("token") || sessionStorage.setItem("token");
 };
